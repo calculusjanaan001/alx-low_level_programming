b87e07f139933b5b49d63106a2eedfc20894ecd8
caculus <calculus>
2023-05-22 11:53:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-  2   2 gcc -E $CFILE -o 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle.\n");
+	return (0);
+/* Have printing and returning zero */
+}

eb2babdc651f2e841a909ff75145a5d8392b59aa
caculus <calculus>
2023-05-22 11:57:53 -0700

    correcting 4-puts.c error-period

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 /* Have printing and returning zero */
 }
